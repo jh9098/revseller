af45b960a7afbdc6378231cdc4c18b6a3638d54e
choi jihoon <jihoon6432@naver.com>
2025-06-27 12:32:35 +0900

    fix: add paths alias, client components, css path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
-  images: { domains: ['firebasestorage.googleapis.com'] }
+  images: { domains: ['firebasestorage.googleapis.com'] },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
