1e2891b2301c0ffd0282a30c67e1b2f95854d3fd
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-07-11 18:18:51 +0900

    Adjust viewport for desktop-like view

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <script src="https://js.tosspayments.com/v1/payment-widget"></script>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=1024" />
       </Head>
       <body>
         <Main />
